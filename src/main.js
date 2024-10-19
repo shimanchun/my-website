2c46419bbe1482a65431059f936c9020728b2b2f
shimachun <sj8504415@gmail.com>
2024-10-20 05:09:44 +0900

    PC fix

--- src/main.js
+++ src/main.js
@@ -7,11 +7,23 @@ function adjustBackgroundScale() {
 
 function updateTransforms(scaleSky, scaleCloud) {
     const offset = window.pageYOffset;
-     document.querySelector('.back-sky [data-multiple] [data-movement] span').style.transform = `translateZ(-2px) translateY(-${offset * 0.2}px) scale(1.5)`;
-     document.querySelector('.back-cloud [data-multiple] [data-movement] span').style.transform = `translateZ(-1px) translateY(-${offset * 0.5}px) scale(1.2)`;
-     document.querySelector('.front-cloud [data-multiple] [data-movement] span').style.transform = `translateZ(0px) translateY(-${offset}px) scale(1)`;
+    const offset1 = -offset + 300;
+    const offset2 = -offset + 400;
+    -offset;
+    document.querySelector('.back-sky [data-multiple] [data-movement] span').style.transform = `translateZ(-2px) translateY(${offset * 0.2}px) scale(1)`;
+    document.querySelector('.back-cloud [data-multiple] [data-movement] span').style.transform = `translateZ(-1px) translateY(${offset2 * 0.5}px) scale(1.1)`;
+    document.querySelector('.front-cloud [data-multiple] [data-movement] span').style.transform = `translateZ(0px) translateY(${offset1}px) scale(1)`;
 }
 
 window.addEventListener('resize', adjustBackgroundScale);
 document.addEventListener('scroll', () => updateTransforms(window.innerWidth / 1920, window.innerWidth / 1920));
 adjustBackgroundScale(); // 初期読み込み時にも実行
+
+anime({
+    targets: '.hero h1, .hero p',
+    translateY: [50, 0], // 30pxから0pxへ移動
+    opacity: [0, 1], // 透明から不透明へ
+    //delay: anime.stagger(1000), // 各ターゲットのアニメーション開始を100ミリ秒ずらす
+    duration: 4000, // 1000ミリ秒（1秒）でアニメーション完了
+    easing: 'easeOutExpo' // イージング関数
+});
